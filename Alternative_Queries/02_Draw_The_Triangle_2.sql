5b6b97364133295d972923a4ef81859f67b293bd
Engy Refaai <138628371+EngyRefaai@users.noreply.github.com>
2025-08-09 23:16:32 +0300

    Create 02_Draw_The_Triangle_2.sql

---
+++ Alternative_Queries/02_Draw_The_Triangle_2.sql
@@ -0,0 +1,8 @@
+WITH RECURSIVE pattern AS (
+    SELECT 1 AS row_num, CAST(REPEAT('* ', 1) AS CHAR(1000)) AS stars
+    UNION ALL
+    SELECT row_num + 1, CAST(REPEAT('* ', row_num + 1) AS CHAR(1000))
+    FROM pattern
+    WHERE row_num < 20
+)
+SELECT stars FROM pattern;
