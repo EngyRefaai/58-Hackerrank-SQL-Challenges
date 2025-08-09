fd2977851df35a81a0ccaf26f08750aab647abe6
Engy Refaai <138628371+EngyRefaai@users.noreply.github.com>
2025-08-09 23:17:36 +0300

    Update 02_Draw_The_Triangle_2.sql

--- Alternative_Queries/02_Draw_The_Triangle_2.sql
+++ Alternative_Queries/02_Draw_The_Triangle_2.sql
@@ -1,8 +1,9 @@
-WITH RECURSIVE pattern AS (
+WITH RECURSIVE triangle AS (
     SELECT 1 AS row_num, CAST(REPEAT('* ', 1) AS CHAR(1000)) AS stars
     UNION ALL
     SELECT row_num + 1, CAST(REPEAT('* ', row_num + 1) AS CHAR(1000))
-    FROM pattern
+    FROM triangle
     WHERE row_num < 20
 )
-SELECT stars FROM pattern;
+SELECT stars 
+FROM triangle;
